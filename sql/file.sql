02248e61dd94301d13917f6a714edb4d153d8c42
Александра Грибова <sasha.gribova@bk.ru>
2022-10-20 12:49:34 +0700

    new wersion

--- sql/file.sql
+++ sql/file.sql
@@ -10,13 +10,13 @@ CREATE TABLE Section
     PRIMARY KEY(id_section)
 );
 
-CREATE TABLE ProductSection
+CREATE TABLE Product_section
 (
 	id_section INT,
     id_product INT
 );
 
-CREATE TABLE FullProduct
+CREATE TABLE Full_product
 (
 	id_product INT AUTO_INCREMENT,
     title VARCHAR(30),
@@ -24,17 +24,19 @@ CREATE TABLE FullProduct
     price_without_discont DECIMAL(7,2),
     price_with_promo DECIMAL(7,2),
     description_product TEXT,
+    main_section INT,
+    is_active BOOL,
     PRIMARY KEY(id_product)
 );
 
-CREATE TABLE ProductOtherImg
+CREATE TABLE Product_other_img
 (
 	id_product INT,
     id_img INT
 );
 
 
-CREATE TABLE ProductMainImg
+CREATE TABLE Product_main_img
 (
 	id_product INT,
     id_img INT
@@ -45,5 +47,6 @@ CREATE TABLE Img
 (
 	id_img INT AUTO_INCREMENT,
     img TEXT,
+    alt TEXT,
     PRIMARY KEY(id_img)
 );
